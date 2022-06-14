a39eb443ff19a6be6180e6e9330a9b0936e5d1d2
dzaky1201 <dzakyhdr@gmail.com>
2022-06-14 22:06:33 +0700

    add layout login

--- app/src/main/java/com/binar/secondhand/login/LoginFragment.kt
+++ app/src/main/java/com/binar/secondhand/login/LoginFragment.kt
@@ -1,30 +1,30 @@
-package com.binar.secondhand.akun
+package com.binar.secondhand.login
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.binar.secondhand.R
-import com.binar.secondhand.databinding.FragmentAkunBinding
+import com.binar.secondhand.databinding.FragmentLoginBinding
 
-class AkunFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-    private var _binding: FragmentAkunBinding? = null
+    private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentAkunBinding.inflate(layoutInflater)
+        _binding = FragmentLoginBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.txtMoveToLogin.setOnClickListener {
-            findNavController().navigate(AkunFragmentDirections.actionNavigationAkunToLoginFragment())
+        binding.icBack.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
 
---
+++ app/src/main/java/com/binar/secondhand/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.binar.secondhand.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
