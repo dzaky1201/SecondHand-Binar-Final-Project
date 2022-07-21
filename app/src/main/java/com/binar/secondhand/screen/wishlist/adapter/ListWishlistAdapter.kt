a95cb8312e61cd935841cc57e3e3abbc9370f67f
Andre <andreashappy29@gmail.com>
2022-07-21 23:10:00 +0700

    [Feature] Wishlist Bugfix - Wishlist Date

--- app/src/main/java/com/binar/secondhand/screen/wishlist/adapter/ListWishlistAdapter.kt
+++ app/src/main/java/com/binar/secondhand/screen/wishlist/adapter/ListWishlistAdapter.kt
@@ -26,26 +26,26 @@ class ListWishlistAdapter : ListAdapter<ListWishlist, ListWishlistAdapter.ViewHo
                 binding.tvProductName.text = "-"
                 binding.tvBasePrice.text = item.product.basePrice.toString()
                 binding.tvLocation.text = "Lokasi di " + " -"
-                binding.tvDate.text = item.product.updatedAt.dateformat()
+                binding.tvDate.text = item.createdAt.dateformat()
             }
             else if (item.product.name == ""){
                 binding.tvProductName.text = "-"
                 binding.tvBasePrice.text = item.product.basePrice.toString()
                 binding.tvLocation.text = "Lokasi di " + item.product.location
-                binding.tvDate.text = item.product.updatedAt.dateformat()
+                binding.tvDate.text = item.createdAt.dateformat()
             }
             else if (item.product.location == ""){
                 binding.tvProductName.text = item.product.name
                 binding.tvBasePrice.text = item.product.basePrice.toString()
                 binding.tvLocation.text = "Lokasi di " + " -"
-                binding.tvDate.text = item.product.updatedAt.dateformat()
+                binding.tvDate.text = item.createdAt.dateformat()
             }
             else {
                 binding.apply {
                     binding.tvProductName.text = item.product.name
                     binding.tvBasePrice.text = item.product.basePrice.toString()
                     binding.tvLocation.text = "Deskripsi: " + item.product.description
-                    binding.tvDate.text = item.product.updatedAt.dateformat()
+                    binding.tvDate.text = item.createdAt.dateformat()
                 }
             }
             Glide.with(binding.root).load(item.product.imageUrl)
